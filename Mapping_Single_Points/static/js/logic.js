f81619f781b730962481536deda9a0e976e4cb0e
Laury <lavegora@gmail.com>
2022-06-21 17:42:51 -0500

    mapping single points

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -5,15 +5,18 @@ console.log("working");
 //let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // Create the map object with a center and zoom level.
-let map = L.map("mapid", {
-    center: [
-      40.7, -94.5
-    ],
-    zoom: 4
-  });
+let map = L.map("mapid").setView([34.0522, -118.2437], 14);
+//  Add a marker to the map for Los Angeles, California.
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+L.circleMarker([34.0522, -118.2437],{
+  color: 'black',
+  fillColor: 'yellow',
+  radius:300
+}).addTo(map);
 
-  // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// We create the tile layer that will be the background of our map.
+// To change the map style to dark map need change streets-v11 to dark-v10
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
